667c454fb4950129780cb86ed0afa98534e3c618
Kayra Demir <81981480+kayrademir1@users.noreply.github.com>
2021-04-07 21:31:05 +0300

    Add files via upload
    İt is 7.04.2021's homework.

---
+++ homework/Day 3 Homework.py
@@ -0,0 +1,8 @@
+print("Welcome to Login Application")
+username=str(input("Enter your username>>>"))
+password=str(input("Enter your password\n(Good passwords have 8+ characters.)>>>"))
+if len(password)<=8:
+    print("Good! You can enter this app.")
+else:
+    print("You can enter this app. Your password should have 8+ characters.")
+
